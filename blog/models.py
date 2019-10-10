a5398b890116a8b8e2212e49e5f2eeb388f4c229
Allan <allancc@gmail.com>
2019-10-09 20:47:14 -0400

    Expose Post in admin

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,12 @@ class Post(models.Model):
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        #the -created means the reverse of created
+        #created is assending order
+
+
+    def __str__(self):
+        return self.title
